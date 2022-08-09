8e371dd4cb9cb64e0c89851cd7bca1261b132610
GL0Z <110708723+GL0Z@users.noreply.github.com>
2022-08-09 00:17:25 -0400

    Create jenkins.groovy

---
+++ jenkins.groovy
@@ -0,0 +1,14 @@
+pipeline {
+    agent any
+    stages {
+        stage('hello AWS') {
+            steps {
+                withAWS(region: 'us-east-1', credentials: 'Jenkins-s3') {
+                    sh 'echo "hello KB">hello.txt'
+                    cfnDelete(stack:'S3bucket-policy')
+                  
+                }
+            }
+        }
+    }
+}
