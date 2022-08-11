d111f8f96dedf32cd5e15285fecc3d6bb2b209c9
GL0Z <110708723+GL0Z@users.noreply.github.com>
2022-08-10 23:32:47 -0400

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -9,7 +9,7 @@ pipeline {
                     //cfnDelete(stack:'S3bucket-policy')
                   //cfnDescribe(stack:"dsh-impl1b-ffm-hub-jboss-intelligent-scaling")
                     cfnValidate(file:'creates3.json')
-                    cfnUpdate(stack:'parms-stack', file:'S3template.yaml', paramsFile:'parms3.yaml')
+                    cfnUpdate(stack:'parm-stack', file:'S3template.yaml', params:['BucketName': 'params5-bucket'])
                 }
             }
         }
